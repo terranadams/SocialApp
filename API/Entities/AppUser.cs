7123fe9a8f3b1ee02f7681fd143a9df25154dbb0
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2024-04-10 12:26:57 -0600

    lesson 10

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,7 @@
+namespace API;
+
+public class AppUser {
+
+    public int Id { get; set; } // entity framework needs these to be public so it can get and set these things in our db
+    public string UserName { get; set;} 
+}
