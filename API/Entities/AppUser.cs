544d629e0286cb4c01ca70be06382ba1599974c8
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2024-05-02 10:39:12 -0600

    lesson 33

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,5 +3,14 @@ namespace API.Entities;
 public class AppUser {
 
     public int Id { get; set; } // entity framework needs these to be public so it can get and set these things in our db
+
     public string UserName { get; set;} 
+
+    public byte[] PasswordHash { get; set; }
+
+    public byte[] PasswordSalt { get; set; } // 'dotnet ef migrations add UserPasswordAdded'
+    // check migrations by going to Data/Migrations
+    // then update the database with 'dotnet ef database update'
+    // then check database by going to 
+
 }
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11267dc760ef2e04f6e907dc2022fc33ff2f844a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3451c9baa32d58fc3d757cdcdb28598658667fb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
